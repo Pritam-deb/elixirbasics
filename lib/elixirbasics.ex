65b033f96f9a9d63f160cc4dbcb0e743444326bf
Pritam-debnath <pritamd@geekyants.com>
2023-09-07 09:36:28 +0530

    initialized basic repo for learning elixir

---
+++ lib/elixirbasics.ex
@@ -0,0 +1,18 @@
+defmodule Elixirbasics do
+  @moduledoc """
+  Documentation for `Elixirbasics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirbasics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
